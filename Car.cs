89996b27af147a952a2f93ee57e324c6e4908027
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-21 10:57:00 -0400

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqOverCollections
+{
+    class Car
+    {
+        public string PetName { get; set; } = "";
+        public string Color { get; set; } = "";
+        public int Speed { get; set; }
+        public string Make { get; set; } = "";
+
+    }
+}
